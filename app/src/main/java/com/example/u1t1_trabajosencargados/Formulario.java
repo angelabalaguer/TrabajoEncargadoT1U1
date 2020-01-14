bc39cfaf7c48b6b570276185368563001b0ec7a2
Angela Balaguer <angbalaguerv@upt.pe>
2020-01-14 13:44:56 -0500

    añadiento codigo del formulario

--- app/src/main/java/com/example/u1t1_trabajosencargados/Formulario.java
+++ app/src/main/java/com/example/u1t1_trabajosencargados/Formulario.java
@@ -3,12 +3,40 @@ package com.example.u1t1_trabajosencargados;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Formulario extends AppCompatActivity {
+    EditText edtNombre;
+    EditText edtApellido;
+    EditText edtFechaNaci;
 
+    Spinner spnrSexo;
+    Spinner spnrGrado;
+
+    Button btnejecutar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+        edtNombre=findViewById(R.id.edTNombre);
+        edtApellido=findViewById(R.id.edtApellido);
+        edtFechaNaci=findViewById(R.id.edtFechNaci);
+        spnrSexo=findViewById(R.id.spnrSexo);
+        spnrGrado=findViewById(R.id.spnrGrado);
+        btnejecutar=findViewById(R.id.btnRegistrar);
+
+
+        btnejecutar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }
